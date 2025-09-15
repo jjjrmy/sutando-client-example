5d10d1881bdb98c0419acfce9bdd051c4f2180f6
Jeremy Holstein <jjjrmy@gmail.com>
2025-09-15 17:13:32 -0400

    wrangler

--- sutando.config.cjs
+++ sutando.config.cjs
@@ -1,36 +1,23 @@
-require('dotenv').config();
+const ClientD1 = require("knex-cloudflare-d1");
 
 const connections = {
   default: {
-    client: 'mysql2',
+    client: ClientD1,
     connection: {
-      host: process.env.DB_HOST ?? 'localhost',
-      port: process.env.DB_PORT ?? 3306,
-      user: process.env.DB_USER ?? 'root',
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME ?? 'sutando',
-    }
+      database: "db",
+    },
+    useNullAsDefault: true,
   },
-  secondary: {
-    client: 'pg',
-    connection: {
-      host: process.env.DB_HOST ?? 'localhost',
-      port: process.env.DB_PORT ?? 5432,
-      user: process.env.DB_USER ?? 'postgres',
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME ?? 'sutando',
-    }
-  }
 };
 
 module.exports = {
   connections,
   useNullAsDefault: true,
   migrations: {
-    table: 'migrations',
-    path: './db/migrations',
+    table: "migrations",
+    path: "./db/migrations",
   },
   models: {
-    path: './models'
-  }
-}; 
+    path: "./models",
+  },
+};
