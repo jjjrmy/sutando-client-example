729d3caead40b903f0b54162021a41b9432c0417
Jeremy Holstein <jjjrmy@gmail.com>
2025-09-15 20:24:26 -0400

    new lock

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -42,13 +42,13 @@ export default defineNuxtConfig({
             plugins: [
                 {
                     name: 'mock-database-drivers',
-                    resolveId(id) {
+                    resolveId(id: string) {
                         const driverIds = ['sqlite3', 'better-sqlite3', 'pg', 'pg-query-stream', 'mysql', 'mysql2', 'oracledb', 'tedious'];
                         if (driverIds.includes(id)) {
                             return { id: 'virtual:' + id, external: false };
                         }
                     },
-                    load(id) {
+                    load(id: string) {
                         if (id.startsWith('virtual:')) {
                             return 'export default {};';
                         }
